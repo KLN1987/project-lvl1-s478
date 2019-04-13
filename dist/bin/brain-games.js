345bf9c5fe6db8329a192d7026b56b655771ca65
lev.karpov <nt66@bk.ru>
2019-04-13 12:25:53 +0300

    ignore dist

--- dist/bin/brain-games.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-var _ = require("..");
-
-(0, _.getName)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBOztBQUVBIiwic291cmNlc0NvbnRlbnQiOlsiIyEvdXNyL2Jpbi9lbnYgbm9kZVxuaW1wb3J0IHsgZ2V0TmFtZSB9IGZyb20gJy4uJztcblxuZ2V0TmFtZSgpO1xuIl19
